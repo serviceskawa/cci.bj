8ed3bf7a5977f5b1fd57e62f566cc544a36f20ae
zounhonloick <zounhonloick@gmail.com>
2022-09-11 08:36:18 +0100

    change router configs

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 import { page } from '@/helpers/routeLoader'
 const routes = [
   {
@@ -24,7 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
