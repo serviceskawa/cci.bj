32ee1d232f90244367a724d2e5829fd4f5d65c71
Kévin ZOUNHON <loic@wallyskak.com>
2022-09-28 10:55:28 +0200

    Update footer elements

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,6 +10,12 @@
               </h3>
               <ul role="list" class="mt-4 space-y-4 capitalize">
                 <li>
+                  <a href="https://api.whatsapp.com/send?phone=22991434343&text=Contactez-nous" target="_blank"
+                    class="text-base text-white hover:text-white">
+                    Whatsapp
+                  </a>
+                </li>
+                <li>
                   <a href="https://www.facebook.com/ccibenin" target="_blank"
                     class="text-base text-white hover:text-white">
                     Facebook
@@ -70,24 +76,24 @@
               </h3>
               <ul role="list" class="mt-4 space-y-4">
                 <li>
-                  <span class="text-base text-white hover:text-white">APIEX
-                  </span>
+                  <a href="https://apiex.bj/" target="_blank" class="text-base text-white hover:text-white">APIEX
+                  </a>
                 </li>
                 <li>
-                  <span class="text-base text-white hover:text-white">PARASEP
-                  </span>
+                  <a  href="https://financements-ue.bj/secteur-prive-projet-dappui-au-renforcement-des-acteurs-du-secteur-prive-parasep/" target="_blank" class="text-base text-white hover:text-white">PARASEP
+                  </a>
                 </li>
                 <li>
-                  <span class="text-base text-white hover:text-white">CPCAF
-                  </span>
+                  <a  href="https://cpccaf.org/" target="_blank" class="text-base text-white hover:text-white">CPCAF
+                  </a>
                 </li>
                 <li>
-                  <span class="text-base text-white hover:text-white">DGI Bénin
-                  </span>
+                  <a  href="https://www.impots.bj/" target="_blank" class="text-base text-white hover:text-white">DGI Bénin
+                  </a>
                 </li>
                 <li>
-                  <span class="text-base text-white hover:text-white">Partners
-                  </span>
+                  <a  href="https://sbin.bj/" target="_blank" class="text-base text-white hover:text-white">SBIN SA
+                  </a>
                 </li>
               </ul>
             </div>
@@ -97,13 +103,13 @@
               </h3>
               <ul role="list" class="mt-4 space-y-4">
                 <li>
-                  <a href="https://api.whatsapp.com/send?phone=22991434343&text=Contactez-nous" target="_blank"
+                  <a href="tel:0022991434343" target="_blank"
                     class="text-base text-white hover:text-white">+229 91 43 43 43
                   </a>
                 </li>
                 <li>
-                  <span class="text-base text-white hover:text-white">+229 21 31 12 38
-                  </span>
+                  <a href="tel:022921311238" class="text-base text-white hover:text-white">+229 21 31 12 38
+                  </a>
                 </li>
                 <li>
                   <a href="mailto:info@ccib.bj" target="_blank"
@@ -116,9 +122,11 @@
         </div>
         <div class="mt-8 xl:mt-0">
           <h3 class="text-base font-medium text-white uppercase">google map</h3>
-          <div class="mt-4 space-y-4">
-            <img src="@/assets/carte.svg" class="w-full" alt="">
-          </div>
+          <a href="https://goo.gl/maps/EAxZJ4uErKkxuM9y6" target="_blank" class="mt-4 space-y-4">
+            <!-- <img src="@/assets/carte.svg" class="w-full" alt=""> -->
+
+            <iframe class="w-full border-none mt-4" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7930.648963993172!2d2.438089!3d6.352021!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe4577eef64b135c7!2sChambre%20de%20Commerce%20et%20de%20l&#39;Industrie%20du%20B%C3%A9nin!5e0!3m2!1sfr!2sbj!4v1664354789753!5m2!1sfr!2sbj" width="180" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </a>
         </div>
       </div>
       <div class="mt-8 border-t border-white text-center pt-8">
