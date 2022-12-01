a07a5968dea7532826edb5e20dcc3204c5847c7b
Thuret <tgenougan@gmail.com>
2022-12-01 14:53:21 +0100

    homepage newslette

--- src/components/NewsLettersBox.vue
+++ src/components/NewsLettersBox.vue
@@ -130,6 +130,8 @@ export default {
     async sendRequest() {
       const data = {
         email: this.email,
+        subject: "Appel offre",
+        slug: "",
       };
       try {
         this.on_loading_request = true;
