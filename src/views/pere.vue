ef8a88001032a1f2e8721b772e5ce0758e1bc0ea
kawa <serviceskawa@gmail.com>
2022-12-03 17:31:23 +0100

    update pere

--- src/views/pere.vue
+++ src/views/pere.vue
@@ -331,7 +331,7 @@
                   </svg>
 
                   <div class="ml-3 text-subtitlegray">
-                    <div class="mb-2">(+229) 91 37 33 33</div>
+                    <a href="tel:0022991373333"><div class="mb-2">(+229) 91 37 33 33</div></a>
                     <div class="mb-2">Lundi - Vendredi 08:00 - 17:30</div>
                   </div>
                 </div>
@@ -354,7 +354,7 @@
 
                   <div class="ml-3 text-subtitlegray">
                     <a href="mailto:pere@ccib.bj" class="underline"
-                      >Pere@ccib.bj</a
+                      >pere@ccib.bj</a
                     >
                   </div>
                 </div>
