e509c6cbf737b478f4fe6190a091e66217f77035
ZOUNHON Kévin <zounhonloick@gmail.com>
2022-12-21 15:41:11 +0100

    Update partenaires

--- src/views/partenaires.vue
+++ src/views/partenaires.vue
@@ -23,7 +23,7 @@
               tracking-tight
             "
           >
-             Nos partenaires
+            Nos partenaires
           </h1>
           <p
             class="
@@ -49,7 +49,8 @@
           projets les
           <br />
           plus ambitieux.
-        </p>
+          </p>
+        </div>
       </div>
     </div>
     <div class="max-w-7xl mx-auto px-6 lg:px-10 py-20">
