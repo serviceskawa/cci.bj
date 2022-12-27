99bfd9d5f789a1e933c1be2a4ef1a2e9073fe617
ZOUNHON Kévin <zounhonloick@gmail.com>
2022-12-27 17:38:42 +0100

    Dynamic Document title

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import {
   createRouter,
   createWebHistory,
-  //createWebHashHistory,
 } from 'vue-router';
 import { page } from '@/helpers/routeLoader';
 import { services } from '@/api'
@@ -11,97 +10,154 @@ const routes = [
     path: '/',
     name: 'index',
     component: page('index'),
+    meta: {
+      title: 'Chambre de Commerce et d\'Industrie du Bénin | CCI Bénin'
+    }
   },
   {
     path: '/financement',
     name: 'financement',
     component: page('financement'),
+    meta: {
+      title: 'Financement'
+    }
   },
   {
     path: '/documents-rapports',
     name: 'documents',
     component: page('documents-rapports'),
+    meta: {
+      title: 'Documents et Rapports'
+    }
   },
   {
     path: '/journees-pays',
     name: 'journees-pays',
     component: page('journee-pays'),
+    meta: {
+      title: 'Journées & Pays'
+    }
   },
   {
     path: '/impulse',
     name: 'impulse',
     component: page('impulse'),
+    meta: {
+      title: 'Impulse'
+    }
   },
   {
     path: '/etude-marche',
     name: 'etude-marche',
     component: page('etude-marches'),
+    meta: {
+      title: 'Études & marchés'
+    }
   },
   {
     path: '/aide-montage',
     name: 'aide-montage',
     component: page('aide-montage'),
+    meta: {
+      title: 'Aide et montage'
+    }
   },
   {
     path: '/rdv-experts',
     name: 'rdv-experts',
     component: page('rdv-experts'),
+    meta: {
+      title: 'Rendez-vous des experts'
+    }
   },
   {
     path: '/contact',
     name: 'contact',
     component: page('contact'),
+    meta: {
+      title: 'Contacts'
+    }
   },
   {
     path: '/agenda',
     name: 'agenda',
     component: page('agenda'),
+    meta: {
+      title: 'Agenda'
+    }
   },
   {
     path: '/arbitrage-mediation',
     name: 'arbitrage-mediation',
     component: page('arbitrage-mediation'),
+    meta: {
+      title: 'Arbitrage et médiation'
+    }
   },
   {
     path: '/formations',
     name: 'formations',
     component: page('formations'),
+    meta: {
+      title: 'Formations'
+    }
   },
   {
     path: '/partenaires',
     name: 'partenaires',
     component: page('partenaires'),
+    meta: {
+      title: 'Nos partenaires'
+    }
   },
   {
     path: '/devenir-partenaire',
     name: 'devenir-partenaire',
     component: page('devenir-partenaire'),
+    meta: {
+      title: 'Devenir partenaire'
+    }
   },
   {
     path: '/blog-articles',
     name: 'blog-articles',
     component: page('blog-articles'),
+    meta: {
+      title: 'Blogs & articles'
+    }
   },
   {
     path: '/articles/:id/:slug',
     name: 'read-article',
     component: page('read-article'),
+    meta: {
+      title: 'Article'
+    }
   },
 
   {
     path: '/appels-offres',
     name: 'appels-offres',
     component: page('appels-offres'),
+    meta: {
+      title: 'Appel d\'offres'
+    }
   },
   {
     path: '/pere',
     name: 'pere',
     component: page('pere'),
+    meta: {
+      title: 'P.E.R.E'
+    }
   },
   {
     path: '/institutionnelle',
     name: 'institutionnelle',
     component: page('institutionnelle'),
+    meta: {
+      title: 'Institutionnel'
+    }
   },
 
   // {
@@ -114,84 +170,107 @@ const routes = [
     path: '/faq',
     name: 'faq',
     component: page('faq'),
+    meta: {
+      title: 'FAQ'
+    }
   },
   {
     path: '/chef-entreprise',
     name: 'chef-entreprise',
     component: page('chef-entreprise'),
+    meta: {
+      title: 'Chef d\'entreprise'
+    }
   },
   {
     path: '/cci',
     name: 'cci',
     component: page('cci'),
+    meta: {
+      title: 'Ma CCI'
+    }
   },
   {
     path: '/createur-entreprise',
     name: 'createur-entreprise',
     component: page('createur-entreprise'),
+    meta: {
+      title: 'Créateur d\'entreprise'
+    }
   },
   {
     path: '/services',
     name: 'services',
     component: page('services'),
+    meta: {
+      title: 'Services'
+    }
   },
   {
     path: '/services/specifiques',
     name: 'services-specs',
     component: page('services-specs'),
+    meta: {
+      title: 'Services | Spécifiques'
+    }
   },
   {
     path: '/services/informations-orientations',
     name: 'services-infos',
     component: page('services-infos'),
+    meta: {
+      title: 'Services | Informations-Orientations'
+    }
   },
   {
     path: '/services/formations',
     name: 'services-formations',
     component: page('services-formations'),
+    meta: {
+      title: 'Services | Formations'
+    }
   },
   {
     path: '/investissement',
     name: 'investissement',
     component: page('investissement'),
+    meta: {
+      title: 'Investissement'
+    }
   },
   {
     path: '/parcours-entrepreneur',
     name: 'parcours-entrepreneur',
     component: page('parcours-entrepreneur'),
+    meta: {
+      title: 'Parcours entrepreneur'
+    }
   },
   //{
-    //path: '/mentions-legales',
-    //name: 'legal-mentions',
-    //component: page('legal-mention'),
+  //path: '/mentions-legales',
+  //name: 'legal-mentions',
+  //component: page('legal-mention'),
   //},
   {
     path: '/observatoire-commerce',
     name: 'observatoire-commerce',
     component: page('observatoire-commerce'),
+    meta: {
+      title: 'Observatoire commerce'
+    }
   },
   {
     path: '/:pathMatch(.*)*', name: 'not-found',
     component: () => import('@/views/404.vue'),
     meta: {
-      layout: 'empty'
+      layout: 'empty',
+      title: '404 | Not found'
     }
   }
 ];
 
-/*window.router = new VueRouter({
-  hashbang: false,
-  //abstract: true,
- history: true,
-   mode: 'html5',
- linkActiveClass: 'active',
- transitionOnLoad: true,
- root: '/'
-});*/
-
 const router = createRouter({
   history: createWebHistory(),
-  //history:createWebHashHistory(),
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 }
   },
@@ -199,6 +278,9 @@ const router = createRouter({
 });
 
 router.beforeEach(async (to, from, next) => {
+  if (to.meta !== undefined && to.meta.title !== undefined) {
+    document.title = to.meta.title
+  }
   let configs = sessionStorage.getItem('configs')
   if (configs == undefined || configs == null) {
     await services.get_settings().then((response) => {
